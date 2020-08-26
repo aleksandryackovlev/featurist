059811129bea5a11cfbba6a9559e985c550da682
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-08-27 00:17:05 +0300

    feat(users): implement basic users module

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return "All users"', () => {
+      expect(controller.findAll()).toBe('All users');
+    });
+  });
+});
