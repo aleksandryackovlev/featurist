1392c512bb3c885319b8bcd851260e4cafa32c72
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-08-31 22:30:22 +0300

    feat(applications): generate applications' module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { EtcdModule } from './etcd/etcd.module';
 import { FeaturesModule } from './features/features.module';
+import { ApplicationsModule } from './applications/applications.module';
 
 import config from './config';
 
@@ -16,23 +17,27 @@ console.log(`${process.env.ETCD_HOST}:${process.env.ETCD_PORT}`);
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => (<TypeOrmModuleOptions>{
-        type: configService.get('db.client'),
-        host: configService.get('db.host'),
-        port: configService.get<number>('db.port'),
-        username: configService.get('db.username'),
-        password: configService.get('db.password'),
-        database: configService.get('db.database'),
-        schema: configService.get('db.schema'),
-        ssl: configService.get<boolean>('db.ssl'),
-        entities: ["dist/**/*.entity{.ts,.js}"],
-        synchronize: true,
-      }),
+      useFactory: (configService: ConfigService) =>
+        <TypeOrmModuleOptions>{
+          type: configService.get('db.client'),
+          host: configService.get('db.host'),
+          port: configService.get<number>('db.port'),
+          username: configService.get('db.username'),
+          password: configService.get('db.password'),
+          database: configService.get('db.database'),
+          schema: configService.get('db.schema'),
+          ssl: configService.get<boolean>('db.ssl'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        },
       inject: [ConfigService],
     }),
-    EtcdModule.forRoot({ hosts: `http://${process.env.ETCD_HOST}:${process.env.ETCD_PORT}`}),
+    EtcdModule.forRoot({
+      hosts: `http://${process.env.ETCD_HOST}:${process.env.ETCD_PORT}`,
+    }),
     UsersModule,
     FeaturesModule,
+    ApplicationsModule,
   ],
   providers: [],
 })
