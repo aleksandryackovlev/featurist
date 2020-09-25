d132d307507cce00186b3cb2dbab3b50f264cb67
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-09-25 22:31:53 +0300

    test(e2e): start implementing e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,13 +13,54 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe({
+      skipMissingProperties: false,
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+    }));
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Applications', () => {
+    describe('POST /applications', () => {
+      it('should throw an 400 if invalid body is sent', () => {
+        return request(app.getHttpServer())
+          .post('/applications')
+          .send({ description: 'Some description', test: 'test' })
+          .expect({
+            statusCode: 400,
+            message: [
+              'property test should not exist',
+              'name must be a string',
+              'name should not be empty'
+            ],
+            error: 'Bad Request'
+          })
+          .expect(400);
+      });
+    });
+
+    describe('PUT /applications/:id', () => {
+      it('should throw an 400 if invalid body is sent', () => {
+        return request(app.getHttpServer())
+          .put('/applications/app-1')
+          .send({})
+          .expect({
+            statusCode: 400,
+            message: [
+              'name should not be empty',
+              'name must be a string',
+              'description should not be empty',
+              'description must be a string'
+            ],
+            error: 'Bad Request'
+          })
+          .expect(400);
+      });
+    });
   });
 });
