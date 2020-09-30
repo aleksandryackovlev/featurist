b037e897f160c88dc20dfe867f8663fb304f07e9
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-09-30 15:08:40 +0300

    feat(applications): implement filters for applications' listing

--- src/applications/applications.service.ts
+++ src/applications/applications.service.ts
@@ -1,3 +1,4 @@
+import { Get } from '@nestjs/common';
 import { CrudService } from '../crud/crud.service';
 
 import { Application } from './application.entity';
@@ -10,9 +11,60 @@ export class ApplicationsService extends CrudService({
   CreateDto: CreateApplicationDto,
   UpdateDto: UpdateApplicationDto,
 }) {
-  async find(
-    findApplicationsDto: FindApplicationsDto,
-  ): Promise<FindApplicationsDto> {
-    return findApplicationsDto;
+  @Get()
+  async find(findApplicationsDto: FindApplicationsDto): Promise<Application[]> {
+    const {
+      createdFrom,
+      createdTo,
+      updatedFrom,
+      updatedTo,
+      search,
+      sortBy = 'createdAt',
+      sortDirection = 'desc',
+      offset,
+      limit = 10,
+    } = findApplicationsDto;
+
+    const query = this.repository.createQueryBuilder('application');
+
+    if (createdFrom) {
+      query.where('CAST (application.createdAt AS DATE) >= :createdFrom', {
+        createdFrom,
+      });
+    }
+
+    if (createdTo) {
+      query.where('CAST (application.createdAt AS DATE) <= :createdTo', {
+        createdTo,
+      });
+    }
+
+    if (updatedFrom) {
+      query.where('CAST (application.updatedAt AS DATE) >= :updatedFrom', {
+        updatedFrom,
+      });
+    }
+
+    if (updatedTo) {
+      query.where('CAST (application.updatedAt AS DATE) <= :updatedTo', {
+        updatedTo,
+      });
+    }
+
+    if (search) {
+      query.where('application.name LIKE :search', { search: `%${search}%` });
+    }
+
+    if (offset) {
+      query.offset(offset);
+    }
+
+    return query
+      .orderBy(
+        `application.${sortBy}`,
+        <'ASC' | 'DESC'>sortDirection.toUpperCase(),
+      )
+      .limit(limit)
+      .getMany();
   }
 }
