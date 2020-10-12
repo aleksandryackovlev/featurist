ab9f59625e04548c3a0ca5cfd520391bd54a5e0c
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-10-12 23:20:49 +0300

    test(auth): fix auth module tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
 
-describe('AuthController', () => {
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+const response = {
+  access_token: 'token',
+};
+
+const service = {
+  login: jest.fn().mockResolvedValue(response),
+};
+
+describe('UsersController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: service,
+        },
+      ],
     }).compile();
+
+    jest.clearAllMocks();
 
     controller = module.get<AuthController>(AuthController);
   });
@@ -15,4 +33,10 @@ describe('AuthController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  // describe('login', () => {
+  //   it('should login users', async () => {
+
+  //   });
+  // });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from '../users/users.service';
+
 import { AuthService } from './auth.service';
+
+const user = {
+  password: 'password',
+  username: 'username',
+  id: 'some-id',
+};
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {
+            findByUsername: jest.fn().mockReturnValue(user),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('access_token'),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
