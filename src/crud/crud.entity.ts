2d102fde38464756aaf817ce5af96987c55a8484
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-10-14 23:24:11 +0300

    docs(openapi): update openapi spec for crud entities

--- src/crud/crud.entity.ts
+++ src/crud/crud.entity.ts
@@ -4,31 +4,31 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptions } from '@nestjs/swagger';
 
 export class CrudEntity {
   @PrimaryGeneratedColumn('uuid')
-  @ApiProperty({
-    example: '8e9ee753-b46d-4241-9518-6731e7d844e9',
+  @ApiProperty(<ApiPropertyOptions>{
     description: 'The unique identifier',
+    'x-faker': 'random.uuid',
   })
   id: string;
 
   @CreateDateColumn({
     name: 'created_at',
   })
-  @ApiProperty({
-    example: '2020-10-09T06:48:59.993Z',
+  @ApiProperty(<ApiPropertyOptions>{
     description: 'The date of the creation',
+    'x-faker': 'date.past',
   })
   createdAt: Date;
 
   @UpdateDateColumn({
     name: 'updated_at',
   })
-  @ApiProperty({
-    example: '2020-10-09T06:48:59.993Z',
+  @ApiProperty(<ApiPropertyOptions>{
     description: 'The date of the last update',
+    'x-faker': 'date.recent',
   })
   updatedAt: Date;
 }
