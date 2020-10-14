85b7af717b09d56fa956fd203ffea2c7bc524d10
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-10-14 08:51:09 +0300

    test(features): test default params for find method in the service

--- src/features/features.service.spec.ts
+++ src/features/features.service.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
-import { Etcd3 } from '../etcd';
 import { ApplicationsService } from '../applications/applications.service';
+import { ETCD_CONNECTION } from '../etcd/etcd.constants';
 
 import { FindFeaturesDto } from './dto/find-features.dto';
 
 import { FeaturesService } from './features.service';
 import { Feature } from './feature.entity';
 
-const feature = new Feature();
-feature.id = '935a38e8-ec14-41b8-8066-2bc5c818577a';
-feature.description = 'John Doe';
-feature.name = 'Description';
+const feature = {
+  id: '935a38e8-ec14-41b8-8066-2bc5c818577a',
+  description: 'John Doe',
+  name: 'feature_name',
+};
 
 const resultArr = [feature];
 
@@ -25,56 +26,85 @@ const query = {
   getMany: jest.fn().mockReturnValue(resultArr),
 };
 
+const etcd = {
+  getAll: jest.fn().mockReturnValue({
+    prefix: jest.fn().mockReturnValue({
+      strings: jest.fn().mockResolvedValue({
+        'appId/feature_name': '1',
+      }),
+    }),
+  }),
+  put: jest.fn().mockReturnValue({
+    value: jest.fn().mockResolvedValue(true),
+  }),
+  get: jest.fn().mockReturnValue({
+    string: jest.fn().mockResolvedValue('1'),
+  }),
+  delete: jest.fn().mockReturnValue({
+    key: jest.fn().mockResolvedValue(true),
+  }),
+};
+
 describe('FeaturesService', () => {
-  // let service: FeaturesService;
+  let service: FeaturesService;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [
-  //       FeaturesService,
-  //       {
-  //         provide: getRepositoryToken(Feature),
-  //         useValue: {
-  //           createQueryBuilder: jest.fn().mockReturnValue(query),
-  //         },
-  //       },
-  //       {
-  //         provide: ApplicationsService,
-  //         useValue: {
-  //           isApplicationExists: jest.fn().mockResolvedValue(query),
-  //         },
-  //       },
-  //       // {
-  //       //   provide: Etcd3,
-  //       //   useValue: {
-  //       //     getAll: jest.fn().mockResolvedValue(query),
-  //       //   },
-  //       // },
-  //     ],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FeaturesService,
+        {
+          provide: getRepositoryToken(Feature),
+          useValue: {
+            createQueryBuilder: jest.fn().mockReturnValue(query),
+          },
+        },
+        {
+          provide: ApplicationsService,
+          useValue: {
+            isApplicationExists: jest.fn().mockResolvedValue(query),
+          },
+        },
+        {
+          provide: ETCD_CONNECTION,
+          useValue: etcd,
+        },
+      ],
+    }).compile();
 
-  //   jest.clearAllMocks();
+    jest.clearAllMocks();
 
-  //   service = module.get<FeaturesService>(FeaturesService);
-  // });
+    service = module.get<FeaturesService>(FeaturesService);
+  });
 
-  // describe('find', () => {
-  //   it('should query the repository with the default params if no args are given', () => {
-  //     expect(service.find('appId', <FindFeaturesDto>{})).resolves.toEqual(resultArr);
+  describe('find', () => {
+    it('should query the repository with the default params if no args are given', async () => {
+      await expect(service.find('appId', <FindFeaturesDto>{})).resolves.toEqual(
+        [
+          {
+            id: '935a38e8-ec14-41b8-8066-2bc5c818577a',
+            description: 'John Doe',
+            name: 'feature_name',
+            isEnabled: true,
+          },
+        ],
+      );
 
-  //     expect(query.where).toBeCalledTimes(0);
-  //     expect(query.andWhere).toBeCalledTimes(0);
-  //     expect(query.offset).toBeCalledTimes(0);
+      expect(query.where).toBeCalledTimes(1);
+      expect(query.where).toBeCalledWith('feature.applicationId = :appId', {
+        appId: 'appId',
+      });
+      expect(query.andWhere).toBeCalledTimes(0);
+      expect(query.offset).toBeCalledTimes(0);
 
-  //     expect(query.orderBy).toBeCalledTimes(1);
-  //     expect(query.orderBy).toBeCalledWith('feature.createdAt', 'DESC');
+      expect(query.orderBy).toBeCalledTimes(1);
+      expect(query.orderBy).toBeCalledWith('feature.createdAt', 'DESC');
 
-  //     expect(query.limit).toBeCalledTimes(1);
-  //     expect(query.limit).toBeCalledWith(10);
+      expect(query.limit).toBeCalledTimes(1);
+      expect(query.limit).toBeCalledWith(10);
 
-  //     expect(query.getMany).toBeCalledTimes(1);
-  //   });
-  // })
+      expect(query.getMany).toBeCalledTimes(1);
+    });
+  });
   it('dummy test', () => {
     expect(true).toEqual(true);
   });
