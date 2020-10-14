e7ec8d088b09db86ac79a396fdfd6215e90f5c45
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2020-10-14 23:23:26 +0300

    docs(openapi): exclude auth endpoints from the openapi spec

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 import { AuthLocalGuard } from './guards/auth.local.guard';
 import { AuthService } from './auth.service';
@@ -7,6 +8,7 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiExcludeEndpoint()
   @UseGuards(AuthLocalGuard)
   @Post('login')
   async login(@Request() req) {
