fcc0114ea50f1047554dd2ad3f70fd8c5d2f217c
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-11 23:19:32 +0300

    feat(users): validate username on create and update

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,10 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[a-zA-Z][-._a-zA-Z\d]{3,}[a-zA-Z\d]$/)
   @ApiProperty({
     example: 'username',
     description: 'The username of a new user',
@@ -12,6 +13,7 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[A-Za-z\d@$!%*#?&]{5,}$/)
   @ApiProperty({
     example: 'somePassword',
     description: 'The password of a new user',
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,9 +1,10 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsString()
   @IsNotEmpty()
+  @Matches(/^[A-Za-z\d@$!%*#?&]{5,}$/)
   @ApiProperty({
     example: 'somePassword',
     description: 'User password',
