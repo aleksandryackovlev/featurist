79bde67f48e0a7c35c196bcee734109c27b16dea
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-11 23:20:09 +0300

    feat(feature): validate feature names on creation

--- src/features/dto/create-feature.dto.ts
+++ src/features/dto/create-feature.dto.ts
@@ -1,9 +1,10 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFeatureDto {
   @IsNotEmpty()
   @IsString()
+  @Matches(/^[a-zA-Z][-._a-zA-Z\d]{3,}[a-zA-Z\d]$/)
   @ApiProperty({
     example: 'Feature name',
     description: 'The name of a new feature',
