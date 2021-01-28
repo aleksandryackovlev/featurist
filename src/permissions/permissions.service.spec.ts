80c7094a61d6992cc02bea9368cdae1fc40d567e
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-29 00:08:02 +0300

    test(permissions): add tests for the permissions service

--- src/permissions/permissions.service.spec.ts
+++ src/permissions/permissions.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { PermissionsService } from './permissions.service';
+import { Permission } from './permission.entity';
+
+const permission = new Permission();
+permission.id = '935a38e8-ec14-41b8-8066-2bc5c818577a';
+permission.action = 'read';
+permission.subject = 'Application';
+permission.roleId = '935a38e8-ec14-41b8-8066-2bc5c818577a';
+
+const resultArr = [permission];
 
 describe('PermissionsService', () => {
   let service: PermissionsService;
+  let repo: Repository<Permission>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PermissionsService],
+      providers: [
+        PermissionsService,
+        {
+          provide: getRepositoryToken(Permission),
+          useValue: {
+            find: jest.fn().mockResolvedValue(resultArr),
+          },
+        },
+      ],
     }).compile();
 
+    jest.clearAllMocks();
+
     service = module.get<PermissionsService>(PermissionsService);
+    repo = module.get<Repository<Permission>>(getRepositoryToken(Permission));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('getPermissionsByRoleId', () => {
+    it('should return permissions by the given role id', async () => {
+      await expect(
+        service.getPermissionsByRoleId('some-role-id'),
+      ).resolves.toEqual(resultArr);
+
+      expect(repo.find).toBeCalledTimes(1);
+      expect(repo.find).toBeCalledWith({
+        select: ['action', 'subject'],
+        where: {
+          roleId: 'some-role-id',
+        },
+      });
+    });
   });
 });
