3b372c97832312f41cd2696e6c8a804b1993144e
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-01-28 22:59:41 +0300

    feat(roles): add permisions checking for roles' routes

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -19,6 +19,10 @@ import {
 
 import { AuthJwtGuard } from '../auth/guards/auth.jwt.guard';
 
+import { PoliciesGuard } from '../permissions/guards/permissions.policies.guard';
+import { CheckPolicies } from '../permissions/decorators/permissions.check-policies';
+import { AppAbility } from '../permissions/permissions-ability.factory';
+
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import { FindRolesDto } from './dto/find-roles.dto';
@@ -46,6 +50,8 @@ export class RolesController {
     type: RolesListResponse,
   })
   @ApiResponse({ status: 400, description: 'Invalid search parameters' })
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can('read', 'Role'))
   async find(@Query() findRolesDto: FindRolesDto): Promise<RolesListResponse> {
     const { data, total } = await this.service.find(findRolesDto);
 
@@ -62,6 +68,8 @@ export class RolesController {
     description: 'The role',
     type: RoleSingleResponse,
   })
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can('read', 'Role'))
   async findOne(@Param('id') id: string): Promise<RoleSingleResponse> {
     return new RoleSingleResponse(await this.service.findOne(id));
   }
@@ -77,6 +85,8 @@ export class RolesController {
     type: RoleSingleResponse,
   })
   @ApiResponse({ status: 400, description: 'Invalid request' })
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can('create', 'Role'))
   async create(
     @Body() createRoleDto: CreateRoleDto,
   ): Promise<RoleSingleResponse> {
@@ -94,6 +104,8 @@ export class RolesController {
     type: RoleSingleResponse,
   })
   @ApiResponse({ status: 400, description: 'Invalid request' })
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can('update', 'Role'))
   async update(
     @Param('id') id: string,
     @Body() updateRoleDto: UpdateRoleDto,
@@ -112,6 +124,8 @@ export class RolesController {
     type: RoleSingleResponse,
   })
   @ApiResponse({ status: 400, description: 'Invalid id' })
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can('delete', 'Role'))
   async remove(@Param('id') id: string): Promise<RoleSingleResponse> {
     return new RoleSingleResponse(await this.service.remove(id));
   }
