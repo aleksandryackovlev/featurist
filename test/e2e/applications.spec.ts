787e3a0527df81d7ee87fc868b7e288e3f175a7f
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-02-02 17:49:26 +0300

    test(e2e): test applications list basic behavior

--- test/e2e/applications.spec.ts
+++ test/e2e/applications.spec.ts
@@ -1,20 +1,79 @@
 import * as request from 'supertest';
 
-describe('Applications (e2e)', () => {
+const app = request('http://localhost:3000');
+
+describe('Applications', () => {
   let adminToken = '';
-  const developerToken = '';
+  let developerToken = '';
+  let restrictedUserToken = '';
+
+  const appsList = [
+    {
+      id: 'c6101e77-9bb8-4756-9720-82677d1b92a5',
+      createdAt: '2021-02-02T13:52:51.084Z',
+      updatedAt: '2021-02-02T13:52:51.084Z',
+      name: 'application_1',
+      description: '<p>Enim qui odit voluptates labore et omnis nisi veritatis. Odit aut et. Incidunt fugiat minima ea. Atque quia quam sed voluptatem.</p>'
+    },
+    {
+      id: '71c02296-5418-4cdd-ace3-3886ae6cdcb3',
+      createdAt: '2021-01-02T13:52:51.065Z',
+      updatedAt: '2021-01-02T13:52:51.065Z',
+      name: 'application_2',
+      description: '<p>Quo minima recusandae minus error qui quod nihil et doloribus. Aspernatur aut sed expedita quis consectetur eveniet. Vitae aperiam quibusdam ab in est est illo. Ut tempora laudantium consequatur quasi eaque aliquam. Voluptate incidunt vitae alias facilis sit quia. Delectus ipsum qui molestiae rem quia eum aliquam.</p>'
+    }
+  ];
 
   beforeAll(async () => {
-    const result = await request('http://localhost:3000')
-      .post('/auth/login')
-      .send({ username: 'admin', password: 'test' });
+    const [admin, developer, restrictedUser] = await Promise.all([
+      app
+        .post('/auth/login')
+        .send({ username: 'admin', password: 'test' }),
+      app
+        .post('/auth/login')
+        .send({ username: 'developer', password: 'test' }),
+      app
+        .post('/auth/login')
+        .send({ username: 'manager', password: 'test' }),
+    ]);
 
-    adminToken = result.body.data.access_token;
+    adminToken = admin.body.data.access_token;
+    developerToken = developer.body.data.access_token;
+    restrictedUserToken = restrictedUser.body.data.access_token;
+  });
+
+  describe('GET /applications', () => {
+    it('should throw an 403 if the current user is not allowed to read applications', () => {
+      return app
+        .get('/applications')
+        .set({
+          Authorization: `Bearer ${restrictedUserToken}`
+        })
+        .expect({
+          statusCode: 403,
+          error: 'Forbidden',
+          message: 'Forbidden resource',
+        })
+        .expect(403);
+    });
+
+    it('should return a list of available applications', () => {
+      return app
+        .get('/applications')
+        .set({
+          Authorization: `Bearer ${adminToken}`
+        })
+        .expect({
+          data: appsList,
+          total: appsList.length,
+        })
+        .expect(200);
+    });
   });
 
   describe('POST /applications', () => {
     it('should throw an 400 if invalid body is sent', () => {
-      return request('http://localhost:3000')
+      return app
         .post('/applications')
         .send({ description: 'Some description', test: 'test' })
         .set({
@@ -35,7 +94,7 @@ describe('Applications (e2e)', () => {
 
   describe('PUT /applications/:id', () => {
     it('should throw an 400 if invalid body is sent', () => {
-      return request('http://localhost:3000')
+      return app
         .put('/applications/app-1')
         .send({})
         .set({
