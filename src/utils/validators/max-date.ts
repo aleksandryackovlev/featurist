44a24e1a4e3fcd2d33e21b970fa0600793ea8cf9
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-02-12 23:40:25 +0300

    feat(utils): add max date validator decorator

---
+++ src/utils/validators/max-date.ts
@@ -0,0 +1,45 @@
+import {
+  registerDecorator,
+  buildMessage,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
+
+export function MaxDate(date: Date, validationOptions?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'maxDate',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [date],
+      options: validationOptions,
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          if (typeof value !== 'string') {
+            return false;
+          }
+          let date: Date;
+          console.log(value, /^[0-9]{4}\-[0-9]{2}\-[0-9]{2}$/.test(value));
+
+          try {
+            date = new Date(value);
+          } catch (e) {
+            return false;
+          }
+
+          return (
+            date instanceof Date &&
+            date.getTime() <= (<Date>args.constraints[0]).getTime()
+          );
+        },
+        defaultMessage: buildMessage(
+          (eachPrefix) =>
+            'maximal allowed date for ' +
+            eachPrefix +
+            '$property is $constraint1',
+          validationOptions,
+        ),
+      },
+    });
+  };
+}
