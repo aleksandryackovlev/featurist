d00efd25c116a2259a9faf40facc33d0df65cc6b
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-02-16 21:26:50 +0300

    feat(utils): add headers param decorator for validation

---
+++ src/utils/decorators/headers.decorator.ts
@@ -0,0 +1,34 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { ClassType } from 'class-transformer/ClassTransformer';
+import { validateOrReject, ValidationError } from 'class-validator';
+
+export const Headers = createParamDecorator(
+  async (value: ClassType<unknown>, ctx: ExecutionContext) => {
+    const headers = ctx.switchToHttp().getRequest().headers;
+
+    const dto = plainToClass(value, headers, { excludeExtraneousValues: true });
+
+    try {
+      await validateOrReject(dto);
+    } catch (errors) {
+      throw new BadRequestException({
+        statusCode: 400,
+        error: 'Bad Request',
+        message: errors.reduce(
+          (acc, error) => [
+            ...acc,
+            ...(error.constraints ? Object.values(error.constraints) : []),
+          ],
+          [],
+        ),
+      });
+    }
+
+    return dto;
+  },
+);
