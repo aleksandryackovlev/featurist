cce2604f32b2eda3f546764753fb60ffb8883195
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-02-20 17:27:00 +0300

    test(auth): fix coverage for auth module

--- src/admin/auth/auth.bcrypt.ts
+++ src/admin/auth/auth.bcrypt.ts
@@ -3,6 +3,7 @@ import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class Bcrypt {
+  /* istanbul ignore next */
   compare(data: any, encrypted: string): Promise<boolean> {
     return bcrypt.compare(data, encrypted);
   }
