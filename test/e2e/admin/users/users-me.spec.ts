b2ae8a39b5204b5123ff6ca9f1aee3d7de9005e1
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2021-03-09 23:22:37 +0300

    test(e2e): test current user route

---
+++ test/e2e/admin/users/users-me.spec.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+describe('Users', () => {
+  describe('GET /users/me', () => {
+    it('should return the current user with permissions info', async () => {
+      const user = entities.users.find(({ username }) => username === 'developer');
+
+      const result = await app
+        .get(`/users/me`)
+        .set({
+          Authorization: `Bearer ${credentials.developerToken}`
+        });
+
+      expect(result.body).toEqual({
+        data: {
+          username: 'developer',
+          permissions: entities.permissions
+            .filter(({ roleId, isAllowed }) => roleId === user.roleId && isAllowed)
+            .map(({ action, subject }) => ({ action, subject }))
+        },
+      });
+
+      expect(result.statusCode).toEqual(200);
+    });
+  });
+});
